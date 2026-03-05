e892e765b3b38de543e6c6aa40f3f0938f1d4c5c
Yuri Souza <yurisouza1604@gmail.com>
2026-03-05 19:48:51 -0300

    Limit e Offset

--- exercicios/4-refinando-consulta/3-ordenando-e-limitando.sql
+++ exercicios/4-refinando-consulta/3-ordenando-e-limitando.sql
@@ -1,7 +1,23 @@
 -- Desenvolva uma consulta para a tabela "customers" que retorne apenas 10 linhas
 
+SELECT * from customers
+WHERE State IS NOT NULL
+AND Fax IS NOT NULL
+LIMIT 10;
 
 -- Usando a consulta anterior, ordene a coluna "Country" de forma crescente e "FirstName" de forma decrescente
 
+SELECT * from customers
+WHERE State IS NOT NULL
+AND Fax IS NOT NULL
+ORDER BY Country ASC, FirstName DESC
+LIMIT 10;
+
 
 -- Continue usando a consulta anterior, dessa vez retorne 10 resultados, no entando ignore as 3 primeiras linhas
+
+SELECT * from customers
+WHERE State IS NOT NULL
+AND Fax IS NOT NULL
+ORDER BY Country ASC, FirstName DESC
+LIMIT 10 OFFSET 3;
