c055d98da56c6196a507a7b5b01c256ebab07f19
Yuri Souza <yurisouza1604@gmail.com>
2026-03-05 11:09:43 -0300

    Realizando Cosultas SQL

--- exercicios/2-construindo-consultas/2-valores-unicos-alias.sql
+++ exercicios/2-construindo-consultas/2-valores-unicos-alias.sql
@@ -1,7 +1,9 @@
 -- Para a tabela "customers" crie uma consulta que retorne apenas os valores únicos da coluna Country
-
+select DISTINCT Country from customers;
 
 -- Na consulta anterior, adicione a coluna Company para verificar as combinações únicas entre empresas e países
+
+select DISTINCT Country AS País, Company AS Empresa from customers;
 
 
 -- Nesta mesma consulta, atribua um alias com título em português para cada coluna
--- exercicios/2-construindo-consultas/3-condicional.sql
+++ exercicios/2-construindo-consultas/3-condicional.sql
@@ -1,2 +1,15 @@
 -- Escreva uma consulta que retorne o nome por extenso dos estados brasileiros
+SELECT state from customers
+WHERE Country = 'Brazil';
 
+SELECT DISTINCT 
+state AS Estado_Sigla,
+Country as Pais,
+CASE 
+    WHEN state = 'SP' THEN 'São Paulo'
+    WHEN state = 'RJ' THEN 'Rio de Janeiro'
+    WHEN state = 'DF' THEN 'Distrito Federal'
+    ELSE 'Outro Estado'
+END AS Estado_Nome
+FROM customers
+WHERE Country = 'Brazil';
