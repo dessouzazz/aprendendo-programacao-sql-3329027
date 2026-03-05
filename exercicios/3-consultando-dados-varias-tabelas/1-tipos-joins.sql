7760af613f9c391701fbbf49ee162c75ca37f299
Yuri Souza <yurisouza1604@gmail.com>
2026-03-05 18:56:59 -0300

    Treinando sql

--- exercicios/3-consultando-dados-varias-tabelas/1-tipos-joins.sql
+++ exercicios/3-consultando-dados-varias-tabelas/1-tipos-joins.sql
@@ -1,1 +1,9 @@
 -- Crie uma consulta para realizar um left join com as tabelas "albums" e "artists"
+
+SELECT 
+    al.title AS Titulo_do_album,
+    ar.name AS Nome_do_artista
+FROM albums al
+LEFT JOIN artists ar 
+ON al.ArtistId = ar.ArtistId
+ORDER BY al.title;
--- exercicios/3-consultando-dados-varias-tabelas/2-join-e-with.sql
+++ exercicios/3-consultando-dados-varias-tabelas/2-join-e-with.sql
@@ -1,4 +1,40 @@
 -- Crie uma consulta para realizar inner join com as tabelas "tracks", "albums" e "artists"
 
+SELECT 
+  t.TrackId AS ID,
+  t.Name AS Musica,
+  al.Title AS Album,
+  ar.Name AS Artista
+FROM tracks t
+INNER JOIN albums al ON t.AlbumId = al.AlbumId
+INNER JOIN artists ar ON al.ArtistId = ar.ArtistId;
 
 -- Refatore a consulta anterior usando a cláusula "with" e verifique o total de músicas existente na base de terminado artista, ex: Caetano Veloso
+
+SELECT 
+  COUNT(t.TrackId) AS Total_de_musicas,
+  ar.Name AS Artista
+FROM tracks t
+INNER JOIN albums al ON t.AlbumId = al.AlbumId
+INNER JOIN artists ar ON al.ArtistId = ar.ArtistId
+GROUP BY ar.Name;
+
+-----------------------------------------------------
+
+
+WITH musica AS (
+  SELECT 
+    t.TrackId AS ID,
+    t.Name AS Musica,
+    al.Title AS Album,
+    ar.Name AS Artista
+  FROM tracks t
+  INNER JOIN albums al ON t.AlbumId = al.AlbumId
+  INNER JOIN artists ar ON al.ArtistId = ar.ArtistId
+)
+
+SELECT 
+  COUNT(ID) AS Total_de_musicas,
+  Artista
+FROM musica
+WHERE Artista = 'Caetano Veloso';
