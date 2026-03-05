c88d5098d81e76ebeb9c87d775b84be2a7576fa6
Yuri Souza <yurisouza1604@gmail.com>
2026-03-05 19:40:43 -0300

    Consultas

--- exercicios/4-refinando-consulta/1-filtro.sql
+++ exercicios/4-refinando-consulta/1-filtro.sql
@@ -1,1 +1,12 @@
 -- Crie uma consulta para realizar inner join com as tabelas "tracks", "albums" e "artists". Além disso, apresente apenas as músicas do artista que contenha "Nação" no nome e título do álbum que não seja "Da Lama Ao Caos"
+
+SELECT
+  t.TrackId AS id,
+  t.Name AS musica,
+  al.Title AS album,
+  ar.Name AS artista
+FROM tracks t 
+INNER JOIN albums al ON t.AlbumId = al.AlbumId
+INNER JOIN artists ar ON al.ArtistId = ar.ArtistId
+WHERE artista LIKE '%Nação%' 
+AND al.Title NOT LIKE 'Da Lama Ao Caos';
--- exercicios/4-refinando-consulta/2-group-by-e-having.sql
+++ exercicios/4-refinando-consulta/2-group-by-e-having.sql
@@ -1,7 +1,39 @@
 -- Crie uma consulta usando as tabelas "invoices" e "customers" cujo o resultado seja o total de gastos por cliente
 
+SELECT
+  c.CustomerId AS ID,
+  c.FirstName AS Nome,
+  c.LastName AS Sobrenome,
+  SUM(i.Total) AS Total_gasto
+FROM invoices i
+INNER JOIN customers c ON i.customerId = c.CustomerId
+GROUP BY c.CustomerId, c.FirstName, c.LastName
+ORDER BY Total_gasto DESC;
 
 -- Na consulta anterior, insira um filtro para que apresente apenas os clientes que gastaram mais de R$30
 
+SELECT
+  c.CustomerId AS ID,
+  c.FirstName AS Nome,
+  c.LastName AS Sobrenome,
+  SUM(i.Total) AS Total_gasto,
+  COUNT(i.Total) AS Total_de_compras
+FROM invoices i
+INNER JOIN customers c ON i.customerId = c.CustomerId
+GROUP BY c.CustomerId
+HAVING SUM(i.Total) >= 40;
 
 -- Na consulta anterior, insira uma coluna que informe a compra de maior valor feita por cada cliente
+
+SELECT
+  c.CustomerId AS ID,
+  c.FirstName AS Nome,
+  c.LastName AS Sobrenome,
+  SUM(i.Total) AS Total_gasto,
+  COUNT(i.Total) AS Total_de_compras,
+  MAX(i.Total) AS Maior_compra
+FROM invoices i
+INNER JOIN customers c ON i.customerId = c.CustomerId
+GROUP BY c.CustomerId
+ORDER BY Maior_compra DESC
+LIMIT 5;
